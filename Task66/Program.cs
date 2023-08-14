f0586117f23f08dafff260b8871e61a308fef1b5
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-08-14 22:45:04 +0500

    Task66 added

---
+++ Task66/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int SumNaturalNumbers (int m, int n)
+{
+    int result = 0;
+    for (int i = m; i<=n; i++) result +=i;
+    return result;
+}
+Console.WriteLine($"M = 10; N = 20; Sum = {SumNaturalNumbers(10,20)}");
+
